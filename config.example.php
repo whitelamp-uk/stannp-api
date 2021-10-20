0be7cc3049a1822bd97352b744deedaba81386c8
Mark Page <mark.page@burdenandburden.co.uk>
2021-10-20 20:13:17 +0000

    Corrects API URL

--- config.example.php
+++ config.example.php
@@ -1,10 +1,10 @@
 <?php
 
-define ( 'STANNP_API_URL',      'https://dash.stannp.com/api/v1/users/me'   );
+define ( 'STANNP_API_URL',      'https://dash.stannp.com/api/v1/'   );
 define ( 'STANNP_API_KEY',      'example_key'       );
 
-define ( 'STANNP_EMAIL_ADMIN',  'admin@my.org'                              );
-define ( 'STANNP_EMAIL_FROM',   'noreply@my.org'                            );
+define ( 'STANNP_EMAIL_ADMIN',  'admin@my.org'                      );
+define ( 'STANNP_EMAIL_FROM',   'noreply@my.org'                    );
 
 define ( 'STANNP_THING',        'example_thing'     );
 
