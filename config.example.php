7ef48f50ca9fa0c9459661e64c06341a953cb1ac
Mark Page <mark.page@burdenandburden.co.uk>
2023-01-17 17:21:07 +0000

    Updates example config

--- config.example.php
+++ config.example.php
@@ -8,15 +8,26 @@ define ( 'STANNP_EMAIL_ADMIN',      'admin@my.org'                      );
 define ( 'STANNP_EMAIL_FROM',       'noreply@my.org'                    );
 
 
-
 // These are not normally used here but within your app config
 
+/*
+
+// Org specific
+define ( 'BLOTTO_SNAILMAIL',            true            );
+define ( 'BLOTTO_SNAILMAIL_PREFIX',     'BWH'           );
+define ( 'BLOTTO_SNAILMAIL_TPL_ANL',    false           ); 
+define ( 'BLOTTO_SNAILMAIL_TPL_WIN',    196677          );
+define ( 'BLOTTO_SNAILMAIL_FROM_ANL',   '2099-12-31'    );
+define ( 'BLOTTO_SNAILMAIL_FROM_WIN',   '2022-05-23'    );
+define ( 'STANNP_ERROR_LOG',            false           );
+
+// Global
 // Postcode matching
-//define ( 'BLOTTO_POSTCODE_PREG',    '^[A-Z][A-Z]?[0-9][A-Z0-9]?[0-9][A-Z][A-Z]$' );
+define ( 'BLOTTO_POSTCODE_PREG',    '^[A-Z][A-Z]?[0-9][A-Z0-9]?[0-9][A-Z][A-Z]$' );
 // POST request timeout seconds
-//define ( 'STANNP_TIMEOUT',          60                  );
+define ( 'STANNP_TIMEOUT',          60                  );
 // Minimum length of left-match to campaign names for redaction limiting
-//define ( 'STANNP_REDACT_SCOPE_LEN', 4                   );
-// Switch on deeper logging
-//define ( 'STANNP_ERROR_LOG',        false               );
+define ( 'STANNP_REDACT_SCOPE_LEN', 4                   );
+
+*/
 
